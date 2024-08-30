ba99029e87a0ab2164f829b8f3a490a41dbba911
christylernz <7918463+christylernz@users.noreply.github.com>
2024-08-31 11:19:55 +1200

    WIP tests for position

--- test/spec/resourcesspec.js
+++ test/spec/resourcesspec.js
@@ -16,22 +16,25 @@ import { GameObject } from "../../resources.js";
 }); */
 
 
-describe("GameObject",() =>{
+describe("Given GameObject", () => {
   it("is defined", () => {
     expect(GameObject).toBeDefined();
   });
-  describe("when created", () =>{
+  describe("when created", () => {
     let gameObject;
-    beforeEach(function() {
-      gameObject = new GameObject("Test");
+    beforeEach(function () {
+      gameObject = new GameObject("Test", [0, 0]);
     });
     it("then it has a valid name", () => {
       expect(gameObject.name).toBe("Test");
       gameObject.name = "Test Object";
       expect(gameObject.name).toBe("Test Object");
-      expect(function() { gameObject.name = "";} ).toThrowError("Name is empty");
-      
+      expect(function () { gameObject.name = ""; }).toThrowError("Name is empty");
+
     });
-    
+    it("then it has a valid position", () => {
+      expect(gameObject.position).toBe([0, 0]);
+    });
+
   })
 })
