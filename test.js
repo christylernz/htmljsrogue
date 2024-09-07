a761932fd0797c9b2b3365a1bcfc14629cf036ea
christylernz <7918463+christylernz@users.noreply.github.com>
2024-09-07 22:55:10 +1200

    adding more examples

--- test.js
+++ test.js
@@ -1,3 +1,52 @@
+//module approach
+//Example 1
+export const MyModule = () => {
+    const externalVariable = 'foo';
+    const internalTrackingVariable = 'bar';
+  
+    const doExternalProcessing = () => {
+      // function to be called by the instantiator
+    }
+  
+    const doInternalProcessing = () => {
+      // internal helper function
+    }
+  
+    return {
+      doExternalProcessing,
+      externalVariable,
+    }
+  }
+  const myInstance = MyModule();
+
+// Example 2 - JavaScript Module Pattern
+const Person = (function () {
+    /**
+     * Private Properties
+     * Even if we declare this variables as var instead of let,
+     * they still are not accessible.
+     */
+    let name;
+    let lastName;
+    return {
+      /**
+       * Public Properties
+       * Everything inside the returned object will be accessible.
+       */
+      setName: function (_name, _lastName) {
+        name = _name;
+        lastName = _lastName;
+      },
+      fullName: function () { return `${name} ${lastName}` }
+    }
+  })();
+  
+  const client = Person;
+  
+  client.setName('John', 'S.');
+  console.log(client.fullName()); // John S.
+  // The name property is not accessible.
+  console.log(client.name); // undefined
 
 
 /*Instead of trying to subclass,
