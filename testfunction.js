7457d0da5ab935bfec5f622a63977c954cec3a2d
christylernz <7918463+christylernz@users.noreply.github.com>
2024-09-28 14:56:07 +1200

    update testfunction.js to properly implement factory functions

--- testfunction.js
+++ testfunction.js
@@ -1,11 +1,39 @@
 export function GameObject(position) {
-  this.position = position;
-  this.render = function () {}
+  
+  const render = function() {};
+  const validatePosition = (position) => {
+      if (Array.isArray(position)) {
+        return position;
+      } else {
+        throw new Error("Position must be an array");
+      }
+    }
+  var position = validatePosition(position);
+  
+  
+  return {
+    get position() {
+        return position;
+      },
+      set position(newPosition) {
+        position = validatePosition(newPosition)
+      },
+    render
+  }
 }
 
 export function SimpleObject(position) {
-  this.position = position;
-  this.render = function () {
+  var gameObject = new GameObject(position);
+  const render = () => {
     return "@";
+  }
+  return {
+    get position() {
+      return gameObject.position;
+    },
+    set position(newPosition) {
+      gameObject.position = newPosition;
+    },
+    render
   }
 }
