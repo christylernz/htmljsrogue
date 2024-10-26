e0d684650f3c01a74f89fd1bc0e7e091b714e807
christylernz <7918463+christylernz@users.noreply.github.com>
2024-10-27 06:57:15 +1300

    Remove GameObject class

--- resources.js
+++ resources.js
@@ -8,60 +8,6 @@ export const icons = {
     floor: '.',
 };
 
-export class GameObject {
-  #name
-  #position
-  #symbol
-  constructor(name, position, symbol){
-    this.#name = this.#validateName(name);
-    this.#position = this.#validatePosition(position);
-    this.#symbol = symbol;
-  }
-  
-  set name(name){
-    this.#name = this.#validateName(name);
-  }
-  
-  get name() {
-    return this.#name;
-  }
-  
-  get position() {
-    return this.#position;
-  }
-  
-  set position(position) {
-    this.#position = this.#validatePosition(position);
-  }
-  
-  get symbol() {
-    return this.#symbol;
-  }
-  
-  #validateName(name){
-    //if (type of n) {}
-    const trimmedName = name.trim();
-    if (trimmedName.length === 0) {
-      throw new Error("Name is empty");
-    }
-    return name;
-  }
-  
-  #validatePosition(position) {
-    if (Array.isArray(position)) {
-      return position;
-    } else {
-      throw new Error("Position must be an array");
-    }
-  }
-  
-  render() {
-    return this.#symbol;
-  }
-  
-  
-  
-}
 
 export function NameProperty(name) {
   let myName;
--- test/spec/resourcesspec.js
+++ test/spec/resourcesspec.js
@@ -1,38 +1,6 @@
 //test/spec/resourcesspec.js
-import { PlayerEntity, NameProperty, GameObject, RenderProperty, PositionProperty} from "../../resources.js";
+import { PlayerEntity, NameProperty, RenderProperty, PositionProperty} from "../../resources.js";
 
-describe("Given GameObject", () => {
-  it("is defined", () => {
-    expect(GameObject).toBeDefined();
-  });
-  describe("when it is created", () => {
-    let gameObject;
-    beforeEach(function () {
-      gameObject = new GameObject("Test", [0, 0], '@');
-    });
-    it("then it has a valid name", () => {
-      expect(gameObject.name).toBe("Test");
-      gameObject.name = "Test Object";
-      expect(gameObject.name).toBe("Test Object");
-      expect(function() { gameObject.name = ""; }).toThrowError("Name is empty");
-
-    });
-    it("then it has a valid position", () => {
-      expect(gameObject.position).toEqual([0, 0]);
-      gameObject.position = [1,1];
-      expect(gameObject.position).toEqual([1,1]);
-      expect(function() { gameObject.position = "test"}).toThrowError("Position must be an array");
-    });
-    it("then it has a symbol", () => {
-      expect(gameObject.symbol).toBe('@');
-    });
-    
-    it("then it renders the symbol", () => {
-      expect(gameObject.render()).toBe('@');
-    });
-    
-  })
-})
 describe("Given PositionProperty", () => {
   it("is defined", () => {
     expect(PositionProperty).toBeDefined();
