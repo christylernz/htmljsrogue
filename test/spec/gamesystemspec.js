1eebd74c52b78a1a02be89d08dc93f30b547844f
christylernz <7918463+christylernz@users.noreply.github.com>
2024-12-07 14:16:34 +1300

    Tidy tests so we only create needed data

--- test/spec/gamesystemspec.js
+++ test/spec/gamesystemspec.js
@@ -12,7 +12,8 @@ describe("Given a player and direction to move", () => {
   
   beforeEach(function () {
     map = [
-      "@"
+      "@",
+      "."
     ];
     bounds = [0,0,map[0].length,map.length];
     direction = 'DOWN';
@@ -24,30 +25,24 @@ describe("Given a player and direction to move", () => {
     };
   });
   it("then player position is updated", () => {
-    map =  [ 
-      "@",
-      "."
-    ];
-    bounds = [0,0,map[0].length,map.length];
     game.movePlayer(player, direction, bounds, map);
     expect(player.move).toHaveBeenCalledWith(direction);
   });
   it("then the move must be within bounds", () => {
+    bounds = [0,0,1,1];
     expect(game.movePlayer(player, 'UP', bounds, map)).toBe(false);
     expect(game.movePlayer(player, 'DOWN', bounds, map)).toBe(false);
     expect(game.movePlayer(player, 'LEFT', bounds, map)).toBe(false);
     expect(game.movePlayer(player, 'RIGHT', bounds, map)).toBe(false);
   });
-  /*it("then the move must not create a collision", () => {
-  
-  });*/
   it("then the move must not create a collision", () => {
     map =  [ 
       "@",
       "#"
     ];
-    bounds = [0,0,map[0].length,map.length];
     expect(game.movePlayer(player, direction , bounds, map)).toBe(false);
   });
+  /*it("then the current player location must be updated", () => {
+  });*/
 });
 
